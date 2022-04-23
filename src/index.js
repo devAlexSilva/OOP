f9eca03237c95b6656c87ec6300a682e14c4ecb7
Alex Silva <9apsilva@gmail.com>
2022-04-23 18:42:38 -0300

    style: change folder structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { engine } from 'express-handlebars'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -8,13 +7,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const port = 2727;
 
 const app = express();
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', `${path.join(__dirname+'/MDN/balls_canvas/views')}`)
 
 app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname+'/MDN/balls_canvas/home.html'))
-    res.render('home');
+    res.sendFile(path.join(__dirname + '/projects_MDN/balls_canvas/home.html'))
 });
 
-app.listen(port, () => console.log(`running in port ${port}`));
+
+app.listen(port, () => console.log(`running in http://localhost:${port}`));
